257d1f54cb44ba9ce66489cc67d142ef819f11ac
Viktor Szépe <viktor@szepe.net>
2020-06-02 14:01:11 +0000

    Add ConcatenationUsageSniff

---
+++ PSR12NeutronRuleset/Sniffs/Strings/ConcatenationUsageSniff.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Prohibit usage of "." operator.
+ */
+
+namespace PSR12NeutronRuleset\Sniffs\Strings;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class ConcatenationUsageSniff implements Sniff
+{
+    /**
+     * @return array<int>
+     */
+    public function register()
+    {
+        return [\T_STRING_CONCAT];
+
+    }
+
+    /**
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $phpcsFile->addError('Logical operator "." is prohibited', $stackPtr, 'NotAllowed');
+    }
+}
