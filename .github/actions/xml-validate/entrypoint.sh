165376d7ca727eeda667ba922942c59881e89da6
Viktor Szépe <viktor@szepe.net>
2023-01-15 01:20:32 +0000

    fix ci

--- .github/actions/xml-validate/entrypoint.sh
+++ .github/actions/xml-validate/entrypoint.sh
@@ -3,8 +3,20 @@
 # Validate XML files.
 #
 
-for INPUT in "${!INPUT_@}"; do
-    echo "INPUT: ${INPUT} = '${!INPUT}'"
-done
+set -e
 
-echo "OK."
+PHPCS_SCHEMA="${INPUT_XML_PHPCS_SCHEMA:-vendor/squizlabs/php_codesniffer/phpcs.xsd}"
+
+# Validate PHPCS schema
+xmllint --noout --schema /tmp/XMLSchema.xsd "${PHPCS_SCHEMA}"
+
+# Validate rulesets
+for INPUT in "${!INPUT_@}"; do
+    if [ "${INPUT}" == INPUT_XML_PHPCS_SCHEMA ]; then
+        continue
+    fi
+
+    test -r "${!INPUT}"
+    xmllint --noout --schema "${PHPCS_SCHEMA}" "${!INPUT}"
+    diff --ignore-blank-lines "${!INPUT}" <(XMLLINT_INDENT="    " xmllint --format "${!INPUT}")
+done
