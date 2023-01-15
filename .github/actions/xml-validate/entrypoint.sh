036ee7171829a9fc695df9549c0d2fa9e3baab6f
Viktor Szépe <viktor@szepe.net>
2023-01-15 09:05:44 +0000

    fix ci

--- .github/actions/xml-validate/entrypoint.sh
+++ .github/actions/xml-validate/entrypoint.sh
@@ -7,18 +7,21 @@ set -e
 
 PHPCS_SCHEMA="${INPUT_XML_PHPCS_SCHEMA:-vendor/squizlabs/php_codesniffer/phpcs.xsd}"
 
+if [ -z "${!INPUT_*}" ]; then
+    echo "No ruleset files specified!"
+    exit 10
+fi
+
 # Validate PHPCS schema
 xmllint --noout --schema /usr/local/share/xml/XMLSchema.xsd "${PHPCS_SCHEMA}"
 
 # Validate rulesets
 for INPUT in "${!INPUT_@}"; do
-    if [ "${INPUT}" == INPUT_XML_PHPCS_SCHEMA ]; then
+    if [ "${INPUT}" == INPUT_XML_PHPCS_SCHEMA ] || [ -z "${!INPUT}" ]; then
         continue
     fi
 
-    set -x
-    test -r "${!INPUT}"
     xmllint --noout --schema "${PHPCS_SCHEMA}" "${!INPUT}"
     # Indentation: 4 spaces
-    diff --ignore-blank-lines "${!INPUT}" <(XMLLINT_INDENT="    " xmllint --format "${!INPUT}")
+    diff --ignore-blank-lines --unified "${!INPUT}" <(XMLLINT_INDENT="    " xmllint --format "${!INPUT}")
 done
