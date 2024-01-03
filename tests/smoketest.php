27ffe133b0bad20316095fa09cdff06d451a0b4b
Viktor Szépe <viktor@szepe.net>
2024-01-03 11:56:14 +0000

    Add compatibility with latest Slevomat

---
+++ tests/smoketest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Smoke test.
+ *
+ * @author User <user@example.com>
+ * @license https://opensource.org/license/gpl-3-0/ GPL-3.0-only
+ * @link https://example.com
+ */
+
+declare(strict_types=1);
+
+return 'OK.';
